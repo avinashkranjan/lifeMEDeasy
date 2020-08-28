5f0ab717c56372fc883e1cd3d791c66976c7215c
Avinash Kr. Ranjan <ranjan.avinash@hotmail.com>
2020-08-28 21:55:38 +0530

    Ambulance

---
+++ src/components/Emergency.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import "./Emergency.css";
+import Ambulance from "../assets/ambulance.svg";
+import { Card, CardBody } from "reactstrap";
+class DoctorRegister extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row  justify-content-center ">
+          <Card className="mt-5 col-12 col-md-6 items">
+            <h1 className="title mt-5">Book an Ambulance</h1>
+            <CardBody>
+              <img src={Ambulance} height="200px" />
+              <form className="mt-5">
+                <input
+                  className="inputitem"
+                  type="text"
+                  placeholder="Enter Your Name"
+                />
+                <input
+                  className="inputitem"
+                  type="email"
+                  placeholder="Enter your Email"
+                />
+                <input
+                  className="inputitem"
+                  type="text"
+                  placeholder="Enter your Emergency"
+                />
+                <textarea
+                  className="inputitem"
+                  type="text"
+                  placeholder="Enter your Location"
+                />
+                <button className="red ripple">Call Ambulance </button>
+              </form>
+            </CardBody>
+          </Card>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DoctorRegister;
